a1ba40337796c3692023caa4b6c893ecb680e631
Oscar Danilo Castelblanco Amaya <ocastelblanco@eci.com.co>
2022-11-02 16:44:46 -0500

    Implementacion clase html 7

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.getElementById('emailMenu');
 const desktopMenu = document.getElementById('menuDesplegable');
+const MenuMovil = document.getElementById('mobileMenu');
+const menuHamburguesa = document.getElementById('menuHamburguesa');
+
 
 menuEmail.addEventListener('click',showDesplegar);
+menuHamburguesa.addEventListener('click',toggleMobileMenu);
 
 function showDesplegar(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    MenuMovil.classList.toggle('inactive');
 }
